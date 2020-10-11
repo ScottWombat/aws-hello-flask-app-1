02042f3e2a03580fa24a04fbf652ecc22df5809a
Revit Hathaisatpong <revit.hathaisatpong@dta.gov.au>
2020-10-11 11:51:33 +1100

    commit 7

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,14 @@ from app.models import User
 def new_user():
     user = User('patkennedy79@gmail.com', 'FlaskIsAwesome')
     return user
+
+
+@pytest.fixture(scope='module')
+def test_client():
+    #flask_app = create_app('flask_test.cfg')
+    flask_app = create_app()
+    # Create a test client using the Flask application configured for testing
+    with flask_app.test_client() as testing_client:
+        # Establish an application context
+        with flask_app.app_context():
+            yield testing_client  # this is where the testing happens!
